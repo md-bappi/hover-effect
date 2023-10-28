9d477ad573a5565ab6b257f7287e578ab606b253
md-bappi <mdabuanasbappy555@gmail.com>
2023-10-28 21:13:21 +0600

    hover effect complete

---
+++ script.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.onmousemove = function (e) {
+    console.log(e);
+    let x = e.pageX - card.offsetLeft;
+    let y = e.pageY - card.offsetTop;
+    
+    card.style.setProperty(`--x`,x + `px`)
+    card.style.setProperty(`--y`,y + `px`)
+  };
+});
